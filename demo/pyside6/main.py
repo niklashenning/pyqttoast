ef9a83368de8993671f36c09518a5307feb18ffb
Niklas Henning <niklashenningbusiness@gmail.com>
2024-03-22 01:13:17 +0100

    Add PySide6 demo

---
+++ demo/pyside6/main.py
@@ -0,0 +1,11 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from window import Window
+
+
+# Run demo
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
