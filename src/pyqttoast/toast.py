21ae7356a911cf44366fa14e53c711d1c50d8a79
Nils <contact@nilssoderman.com>
2025-03-24 13:39:56 +0100

    Import future annotations to support python versions < 3.10

--- src/pyqttoast/toast.py
+++ src/pyqttoast/toast.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import math
 from qtpy.QtGui import QGuiApplication, QScreen
 from qtpy.QtCore import Qt, QPropertyAnimation, QPoint, QTimer, QSize, QMargins, QRect, Signal
